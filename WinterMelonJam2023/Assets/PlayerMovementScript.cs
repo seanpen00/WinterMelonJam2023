9181edbcc3c28ba36d0b2dd4e418a7b019694e17
Sean <69405243+seanpen00@users.noreply.github.com>
2023-12-28 15:10:40 -0500

    Pushing

--- WinterMelonJam2023/Assets/PlayerMovementScript.cs
+++ WinterMelonJam2023/Assets/PlayerMovementScript.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool canDoubleJump;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check if the player is grounded using a small circle at the player's feet.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        // Reset double jump ability when grounded.
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        // Jump input.
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the player horizontally.
+        float moveDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            // Regular jump.
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        else if (canDoubleJump)
+        {
+            // Double jump.
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            canDoubleJump = false;
+        }
     }
 }
