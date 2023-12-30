010f939a712b3e6968b2e6446d7536e62260e7ad
Sean <69405243+seanpen00@users.noreply.github.com>
2023-12-30 18:02:14 -0500

    Added changes

--- WinterMelonJam2023/Assets/Scripts/CameraFollow.cs
+++ WinterMelonJam2023/Assets/Scripts/CameraFollow.cs
@@ -6,17 +6,25 @@ public class CameraFollow : MonoBehaviour
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
-    [SerializeField] private GameObject target;
+    private GameObject target;
     private PlayerGameLogic playerLogic;
+    private Rigidbody2D rb;
     int playerHealth;
 
     private void Start()
     {
-        playerLogic = target.GetComponent<PlayerGameLogic>();
+        target = GameObject.Find("The Kevin");
+        rb = target.GetComponent<Rigidbody2D>();
+
     }
     private void Update()
     {
-        if (playerLogic.HEALTH > 0)
+        if (playerLogic == null)
+        {
+            playerLogic = target.GetComponent<PlayerGameLogic>();
+            rb.constraints = RigidbodyConstraints2D.None | RigidbodyConstraints2D.FreezeRotation;
+        }
+        else if (playerLogic.HEALTH > 0)
         {
             Vector3 targetPosition = target.transform.position + offset;
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
